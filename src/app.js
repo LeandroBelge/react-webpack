1d389cfecf44ba34816fe067dfdff9aae320fe5c
unknown <leandrobelge@gmail.com>
2019-06-18 11:30:22 -0300

    Sexta aula

---
+++ src/app.js
@@ -0,0 +1,10 @@
+'use strict'
+import React from 'react'
+
+var createReactClass = require('create-react-class');
+
+export default createReactClass({
+    render: function(){
+        return React.createElement('h1', null, 'Título')
+    }
+})
--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('webpack funcionando')
+'use strict'
+import Title from './app';
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+ReactDOM.render(
+    React.createElement(Title),
+    document.querySelector('[data-js="app"]')
+
+)
